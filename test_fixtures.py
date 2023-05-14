092998fe46c44c395120629068dd986cc8cde875
ibraz511 <fatai.ibrahim@babbangona.com>
2023-05-14 19:40:20 +0100

    Fixtures in pytest

---
+++ test_fixtures.py
@@ -0,0 +1,13 @@
+import pytest
+@pytest.fixture() # this is to introduce precondition and postcondion
+def Calculate(): # Note that the function name does not begin with test since its not a test case. Calculate () is used as a fixture here
+    print("Before hook is running") # this will run first. This is the before hook
+
+    yield #yeild is to introduce post condition or tearDown/after hook
+    print("After hook is running")
+
+def test_Message(Calculate): #Note that we passed the fixture to he main test case
+    print("I am the main test without any mark and I will run second") # This is the main test
+
+
+#NOTE: The yeild is like after. Any code that is written after yeild keyword will execute last
--- test_login.py
+++
@@ -1,7 +0,0 @@
-def test_FirstAddition():
-    assert 3+4==7,"invalid result"
-
-def test_SecondAddition():
-    assert -3-4==7, "invalid result"
-
-#Note: The assert statement has an else part that is displayed if the assertion fails
