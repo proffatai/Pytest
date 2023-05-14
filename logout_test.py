92df156654a533d8a5db53a5c686686e2c4fd9a3
ibraz511 <fatai.ibrahim@babbangona.com>
2023-05-14 16:37:24 +0100

    Running specific test case using -k flag

---
+++ logout_test.py
@@ -0,0 +1,7 @@
+def testFirstMessage():
+    print(f"This is for valid calculation")
+
+def test_SecondMessage():
+    print("This is for invalid calculation")
+
+
---
+++ test_login.py
@@ -0,0 +1,7 @@
+def test_FirstAddition():
+    assert 3+4==7,"invalid result"
+
+def test_SecondAddition():
+    assert -3-4==7, "invalid result"
+
+#Note: The assert statement has an else part that is displayed if the assertion fails
