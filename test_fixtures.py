51bfe5480a3735ad7a17ba24a7223eb7bfbd3cbe
ibraz511 <fatai.ibrahim@babbangona.com>
2023-05-20 00:57:30 +0100

    Added command to run the script

--- test_fixtures.py
+++ test_fixtures.py
@@ -6,8 +6,10 @@ def Calculate(): # Note that the function name does not begin with test since it
     yield #yeild is to introduce post condition or tearDown/after hook
     print("After hook is running")
 
-def test_Message(Calculate): #Note that we passed the fixture to he main test case
+def test_Message(Calculate): #Note that we passed the fixture to the main test case
     print("I am the main test without any mark and I will run second") # This is the main test
 
 
 #NOTE: The yeild is like after. Any code that is written after yeild keyword will execute last
+
+## Run the code from the location of the file on cmd by using `pytest -v -s`
