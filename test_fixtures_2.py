a8c550088e345386b59b223c41a68360c76248c2
ibraz511 <fatai.ibrahim@babbangona.com>
2023-05-20 04:15:36 +0100

    Passing data from another file to our test case

--- test_fixtures_2.py
+++
@@ -1,6 +0,0 @@
-def test_Message(Calculate): #Note that we passed the fixture to the main test case
-    print("I am the main test from the second test file") # This is the main test
-
-
-#Note: Calculate() was created inside the conftest file so we can call the method name anywhere in our test case
-#Run the code from the location of the file on cmd by using `pytest -v -s`
